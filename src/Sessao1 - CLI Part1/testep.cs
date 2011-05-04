2b03a73c8a667e882f162b6561c0b23ab5c387a6
Luis Zacarias <lzacarias@gmail.com>
2011-05-04 22:27:12 +0100

    Sessao1, parte1

---
+++ src/Sessao1 - CLI Part1/testep.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Sessao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Ponto p1;
+		  Ponto p2;
+		  p1=new Ponto(3,9);
+		  p2=new Ponto(3,4);
+		  
+		  int Res;
+		  Res = p1.Distance(p2);
+		  System.Console.WriteLine("Res= {0} ",Res);
+		  
+		  string Res1 = p2.ToString();
+		  System.Console.WriteLine("Res1= {0} ",Res1);
+		  
+        }
+    }
+}
